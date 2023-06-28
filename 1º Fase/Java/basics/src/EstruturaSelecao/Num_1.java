c749210c8932abbbbeb27521dde7ad3d5e32d2f2
lucas0headshot <lucas0headshot@gmail.com>
2023-06-28 20:38:43 -0300

    Introdução ao Java - Realocação dos arquivos

---
+++ 1º Fase/Java/basics/src/EstruturaSelecao/Num_1.java
@@ -0,0 +1,21 @@
+package EstruturaSelecao;
+
+import java.util.Scanner;
+
+public class Num_1 {
+    
+    public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       
+       char sexo;
+        
+       System.out.print("Digite um sexo(M ou F): ");
+            sexo = read.next().charAt(0);
+       
+        switch (sexo) {
+            case 'M' -> System.out.println(sexo + " = Masculino");
+            case 'F' -> System.out.println(sexo + " = Feminino");
+            default -> System.out.println("Sexo inválido...");
+        }
+    }
+}
---
+++ 1º Fase/Java/basics/src/EstruturaSelecao/Num_2.java
@@ -0,0 +1,22 @@
+package EstruturaSelecao;
+
+import java.util.Scanner;
+
+public class Num_2 {
+    
+    public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       
+       char turno;
+        
+       System.out.print("Em qual turno você estuda(M, V ou N)? ");
+            turno = read.next().charAt(0);
+       
+        switch (turno) {
+            case 'M' -> System.out.println("Opa, então bom dia para você!");
+            case 'V' -> System.out.println("Opa, então boa tarde para você!");
+            case 'N' -> System.out.println("Opa, então boa noite para você!");
+            default -> System.out.println("Sexo inválido...");
+        }
+    }   
+}
