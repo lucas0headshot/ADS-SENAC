596ed092725bac928c5eb73cf3495517665e9eb9
lucas0headshot <lucas0headshot@gmail.com>
2023-08-07 21:59:43 -0300

    Adição Nºs 1, 3, 5, 7 e 9 de 07-08

---
+++ 2º Fase/ED/Atividades 07-08/Question1.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayOfInt = new ArrayList<>();
+
+        Scanner scan = new Scanner(System.in);
+
+        double average = 0;
+        int largerNumber = 0;
+        int smallerNumber = Integer.MAX_VALUE;
+
+
+        //Read 5 numbers
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Type the " + (i + 1) + "º number: ");
+            arrayOfInt.add(scan.nextInt());
+
+            average += arrayOfInt.get(i);
+
+            //Read the > number
+            if(arrayOfInt.get(i) > largerNumber) {
+                largerNumber = arrayOfInt.get(i);
+            }
+
+            //Read the < number
+            if(arrayOfInt.get(i) < smallerNumber) {
+                smallerNumber = arrayOfInt.get(i);
+            }
+        }
+
+        //Calculate the average of the numbers
+        average /= arrayOfInt.size();
+
+        //Print the larger, smaller and avarege of the numbers
+        System.out.println("Larger number: " + largerNumber);
+        System.out.println("Smaller number: " + smallerNumber);
+        System.out.println("Average in " + arrayOfInt + ": " + average);
+
+
+        //Close Scanner
+        scan.close();
+    }
+}
---
+++ 2º Fase/ED/Atividades 07-08/Question3.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Question3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayOfInt = new ArrayList<>();
+
+        Scanner scan = new Scanner(System.in);
+
+        int largerNumber = 0;
+        int smallerNumber = Integer.MAX_VALUE;
+
+
+        //Read 10 numbers
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Type the " + (i + 1) + "º number: ");
+            arrayOfInt.add(scan.nextInt());
+
+            //Read the > number
+            if(arrayOfInt.get(i) > largerNumber) {
+                largerNumber = arrayOfInt.get(i);
+            }
+
+            //Read the < number
+            if(arrayOfInt.get(i) < smallerNumber) {
+                smallerNumber = arrayOfInt.get(i);
+            }
+        }
+
+        System.out.println("Larger number: " + largerNumber + " and are in " + arrayOfInt.indexOf(largerNumber) + "º of array");
+        System.out.println("Smaller number: " + smallerNumber + " and are in " + arrayOfInt.indexOf(smallerNumber) + "º of array");
+        System.out.println("The difference between " + largerNumber + " and " + smallerNumber + " are " + (largerNumber - smallerNumber));
+
+        scan.close();
+    }
+}
