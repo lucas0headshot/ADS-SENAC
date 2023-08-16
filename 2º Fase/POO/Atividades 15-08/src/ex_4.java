a8796ca58fc53a73f016bb66536c6fe7fc024ac4
lucas0headshot <lucas0headshot@gmail.com>
2023-08-15 22:01:46 -0300

    Adição Nºs 3, 4 e 5 de 15-08 em POO

---
+++ 2º Fase/POO/Atividades 15-08/src/ex_4.java
@@ -0,0 +1,15 @@
+//Calculadora simples com JOptionPane
+
+import javax.swing.*;
+
+public class ex_4 {
+    public static void main(String[] args) {
+        Integer num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o primeiro número:", "Calculadora", JOptionPane.INFORMATION_MESSAGE));
+
+        Integer num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o segundo número:", "Calculadora", JOptionPane.INFORMATION_MESSAGE));
+
+
+        Integer soma = num1 + num2;
+        JOptionPane.showMessageDialog(null, num1 + " + " + num2 + " = " + soma, "Calculadora", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
---
+++ 2º Fase/POO/Atividades 15-08/src/ex_5.java
@@ -0,0 +1,14 @@
+//Receber um nome e sobrenome, concatena-los e informar ao usuário
+
+import javax.swing.*;
+
+public class ex_5 {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog(null, "Digite seu nome:", "Informe", JOptionPane.INFORMATION_MESSAGE);
+
+        String sobrenome = JOptionPane.showInputDialog(null, "Digite seu sobrenome:", "Informe", JOptionPane.INFORMATION_MESSAGE);
+
+        String nomeCompleto = nome + " " + sobrenome;
+        JOptionPane.showMessageDialog(null, "Seu nome completo é " + nomeCompleto, "Nome completo", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
