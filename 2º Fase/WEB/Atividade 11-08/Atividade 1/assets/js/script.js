2dc45669ca36562d536807ede28b9f01697f3490
lucas0headshot <lucas0headshot@gmail.com>
2023-08-18 19:58:01 -0300

    Correção Nº1 de 11-08 em WEB

--- 2º Fase/WEB/Atividade 11-08/Atividade 1/assets/js/script.js
+++ 2º Fase/WEB/Atividade 11-08/Atividade 1/assets/js/script.js
@@ -1,13 +1,13 @@
 function calcularSalario(e){
-    let horasTrabalhadas = parseInt(document.getElementById('horas_trabalhadas').value);
-    let valorHora = parseInt(document.getElementById('valor_hora').value);
-    let desconto = parseInt(document.getElementById('desconto').value);
-    let bonus = parseInt(document.getElementById('bonus').value);
+    let horasTrabalhadas = parseFloat(document.getElementById('horas_trabalhadas').value);
+    let valorHora = parseFloat(document.getElementById('valor_hora').value);
+    let desconto = parseFloat(document.getElementById('desconto').value);
+    let bonus = parseFloat(document.getElementById('bonus').value);
     
-    if(horasTrabalhadas < 0 || valorHora < 0 || desconto < 0 || bonus < 0){
+    if(horasTrabalhadas < 0.00 || valorHora < 0.00 || desconto < 0.00 || bonus < 0.00){
         alert('Digite valores válidos!');
     }else{
-        salario = ((horasTrabalhadas * valorHora) + bonus) - desconto;
+        let salario = ((horasTrabalhadas * valorHora) + bonus) - desconto;
         isNaN(salario) ? alert("Digite valores válidos!") : alert("Salário: " + salario);
     }
 }
