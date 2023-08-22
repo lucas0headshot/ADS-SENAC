857c5b5f15cfba652ff5a342f0f7dd8485959a02
lucas0headshot <lucas0headshot@gmail.com>
2023-08-22 19:07:48 -0300

    Correção Nº5 de 22-08 em ED

--- 2º Fase/ED/Atividades 22-08/Questao5.java
+++ 2º Fase/ED/Atividades 22-08/Questao5.java
@@ -18,8 +18,22 @@ public class Questao5 {
         System.out.println("Digite uma palavra: ");
         palavra = scan.nextLine();
 
+        // Transformar a palavra em um array de caracteres
+        char[] caracteres = palavra.toCharArray();
 
-        //Transformar
-        palavra.toCharArray();
+        // Preencher a pilha com os caracteres da palavra
+        for(char c : caracteres){
+            palavraInvertida.append(c);
+        }
+
+        // Inverter a palavra usando o StringBuilder
+        palavraInvertida.reverse();
+        
+        // Verificar se a palavra é um palíndromo
+        if(palavra.equals(palavraInvertida.toString())) {
+            System.out.println(palavra + " é um palíndromo.");
+        }else{
+            System.out.println(palavra + " não é um palíndromo.");
+        }
     }
 }
