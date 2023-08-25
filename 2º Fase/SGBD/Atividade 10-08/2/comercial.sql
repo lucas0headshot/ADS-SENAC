52720e4da2d37922ee06b7399b45d9f3f63ae11e
lucas0headshot <lucas0headshot@gmail.com>
2023-08-24 21:29:00 -0300

    Correção Nº2 de 10-08 em SGBD

--- 2º Fase/SGBD/Atividade 10-08/2/comercial.sql
+++ 2º Fase/SGBD/Atividade 10-08/2/comercial.sql
@@ -17,7 +17,7 @@ USE `db_pedidos` ;
 -- -----------------------------------------------------
 -- Table `db_pedidos`.`representante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_pedidos`.`representante` (
+CREATE TABLE IF NOT EXISTS `db_pedidos`.`representantes` (
   `cd_representante` INT(5) NOT NULL,
   `nm_representante` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`cd_representante`))
@@ -27,7 +27,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `db_pedidos`.`cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_pedidos`.`cliente` (
+CREATE TABLE IF NOT EXISTS `db_pedidos`.`clientes` (
   `cd_cliente` INT(5) NOT NULL,
   `nm_cliente` VARCHAR(100) NOT NULL,
   `nm_cidade` VARCHAR(200) NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `db_pedidos`.`cliente` (
   INDEX `fk_cliente_idx` (`cd_representante` ASC) ,
   CONSTRAINT `fk_cliente`
     FOREIGN KEY (`cd_representante`)
-    REFERENCES `db_pedidos`.`representante` (`cd_representante`)
+    REFERENCES `db_pedidos`.`representantes` (`cd_representante`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -45,7 +45,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `db_pedidos`.`produto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_pedidos`.`produto` (
+CREATE TABLE IF NOT EXISTS `db_pedidos`.`produtos` (
   `cd_produto` INT(10) NOT NULL,
   `nm_produto` VARCHAR(200) NOT NULL,
   `id_referencia` VARCHAR(20) NOT NULL,
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `db_pedidos`.`estoque` (
   INDEX `fk_estoque1_idx` (`cd_produto` ASC) ,
   CONSTRAINT `fk_estoque1`
     FOREIGN KEY (`cd_produto`)
-    REFERENCES `db_pedidos`.`produto` (`cd_produto`)
+    REFERENCES `db_pedidos`.`produtos` (`cd_produto`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -78,7 +78,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `db_pedidos`.`pedido`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_pedidos`.`pedido` (
+CREATE TABLE IF NOT EXISTS `db_pedidos`.`pedidos` (
   `nr_pedido` INT NOT NULL AUTO_INCREMENT,
   `cd_cliente` INT(5) NOT NULL,
   `dt_emissao` DATE NOT NULL,
@@ -89,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `db_pedidos`.`pedido` (
   INDEX `fk_pedido1_idx` (`cd_cliente` ASC) ,
   CONSTRAINT `fk_pedido1`
     FOREIGN KEY (`cd_cliente`)
-    REFERENCES `db_pedidos`.`cliente` (`cd_cliente`)
+    REFERENCES `db_pedidos`.`clientes` (`cd_cliente`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -98,7 +98,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `db_pedidos`.`pedido_produto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_pedidos`.`pedido_produto` (
+CREATE TABLE IF NOT EXISTS `db_pedidos`.`pedido_itens` (
   `nr_pedido` INT NOT NULL,
   `cd_produto` INT(10) NOT NULL,
   `qt_produto` DECIMAL(9,2) NOT NULL,
@@ -108,12 +108,12 @@ CREATE TABLE IF NOT EXISTS `db_pedidos`.`pedido_produto` (
   INDEX `fk_pedido_produto1_idx` (`nr_pedido` ASC) ,
   CONSTRAINT `fk_pedido_produto1`
     FOREIGN KEY (`nr_pedido`)
-    REFERENCES `db_pedidos`.`pedido` (`nr_pedido`)
+    REFERENCES `db_pedidos`.`pedidos` (`nr_pedido`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `fk_pedido_produto2`
     FOREIGN KEY (`cd_produto`)
-    REFERENCES `db_pedidos`.`produto` (`cd_produto`)
+    REFERENCES `db_pedidos`.`produtos` (`cd_produto`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
