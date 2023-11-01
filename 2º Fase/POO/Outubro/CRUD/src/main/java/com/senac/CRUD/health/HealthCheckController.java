109167164f56d8a0e22ac0f42092cabbd4b49154
lucas0headshot <lucas0headshot@gmail.com>
2023-10-31 21:57:43 -0300

    Começo do projeto c/ SpringBoot em POO

---
+++ 2º Fase/POO/Outubro/CRUD/src/main/java/com/senac/CRUD/health/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.senac.CRUD.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
