fc3b2c41f3cf6694642872de0fa31237b1c77e51
lucas0headshot <lucas0headshot@gmail.com>
2023-11-02 16:24:10 -0300

    Adição das functions aos .sql da atividade em SGBD

--- 2º Fase/SGBD/Novembro/Atividades 02-11/Functions/1.sql
+++ 2º Fase/SGBD/Novembro/Atividades 02-11/Functions/1.sql
@@ -1,1 +1,16 @@
+DELIMITER $$
+CREATE OR REPLACE FUNCTION `f_ultimo_pedido`() RETURNS date
+BEGIN
+	DECLARE dt_ultimo_pedido DATE;
+    
+    SELECT
+		MAX(dt_emissao) INTO dt_ultimo_pedido
+	FROM pedidos;
+    
+    RETURN dt_ultimo_pedido;
+END $$
+DELIMITER ;
+
+
+
 SELECT f_ultimo_pedido() AS dt_ultimo_pedido;
--- 2º Fase/SGBD/Novembro/Atividades 02-11/Functions/2.sql
+++ 2º Fase/SGBD/Novembro/Atividades 02-11/Functions/2.sql
@@ -1,1 +1,17 @@
+DELIMITER $$
+CREATE OR REPLACE FUNCTION `f_total_itens`(nr_pedido INT) RETURNS double
+BEGIN
+	DECLARE vl_total DOUBLE;
+	
+    SELECT
+		SUM(vl_item) INTO vl_total
+	FROM pedido_itens
+    WHERE pedido_itens.nr_pedido = nr_pedido;
+
+	RETURN vl_total;
+END $$
+DELIMITER ;
+
+
+
 SELECT f_total_itens(590) AS vl_total_pedido;
